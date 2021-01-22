25622af5eaac3a61ee54c2108a7b3824bd9e44a0
crash138 <67919714+sarah-herren@users.noreply.github.com>
2021-01-22 12:23:16 -0600

    mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,19 +1,31 @@
+import 'regenerator-runtime/runtime'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-import 'regenerator-runtime/runtime'
-const fetchGithub = async () => {
- const result = await axios.get('https://api.github.com/users/sarahdherren')
-  console.log(result)
-}
-fetchGithub()
+
+
+
+// const fetchGithub = async () => {
+//  const result = await axios.get('https://api.github.com/users/sarahdherren')
+//   console.log(result.data.name)
+// }
+
+//const userData = axios.get('https://api.github.com/users/sarahdherren')
+
+
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
-    data in order to use it to build your component function
+    data in order to use it to build your component function */
 
+//console.log(userData)
+
+/*
     Skip to STEP 3.
 */
 
@@ -21,6 +33,16 @@ fetchGithub()
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+const cards = document.querySelector('.cards');
+axios.get('https://api.github.com/users/sarahdherren')
+  .then(res => {
+    console.log(res.data.followers_url)
+    cards.append(userCardCreator(res.data));
+  })
+  .catch(err => {
+    console.log('something went wrong', err);
+  });
+
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -33,63 +55,74 @@ fetchGithub()
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['harvey-magana', 'evllz', 'cbarcinas', 'tetondan'];
+
+followersArray.forEach((login) => {
+  axios.get(`https://api.github.com/users/${login}`)
+    .then(res => {
+      cards.append(userCardCreator(res.data));
+    })
+    .catch(err => {
+      console.log('something went wrong', err);
+    })
+});
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:*/
-function userCardCreator(user) {
-  const card = document.createElement('div')
-  card.classList.add('card')
-  const image = document.createElement('img')
-  image.src = 'data.avatar_url'
-  const cardInfo = document.createElement('div')
-  cardInfo.classList.add('card-info')
-  const name = document.createElement('h3')
-  name.classList.add('name')
-  name.textContent = `${data.name}`
-  const userName = document.createElement('p')
-  userName.classList.add('username')
-  userName.textContent = `${data.username}`
-  const location = document.createElement('p')
-  location.textContent = `${data.location}`
-  const profile = document.createElement('p')
-  profile.textContent = 'Profile:'
-  const gitLink = document.createElement('a')
-  gitLink.textContent = `${data.html_url}`
-  const followers = document.createElement('p')
-  followers.textContent = 'Followers: `${data.followers}`'
-  const following = document.createElement('p')
-  following.textContent = 'Following: `${data.following}`'
-  const bio = document.createElement('p')
-  bio.textContent = 'Bio: `${data.bio}`'
+function userCardCreator(object) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  const image = document.createElement('img');
+  image.src = object.avatar_url;
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  const nameHeader = document.createElement('h3');
+  nameHeader.classList.add('name');
+  nameHeader.textContent = object.name;
+  const userName = document.createElement('p');
+  userName.classList.add('username');
+  userName.textContent = object.login;
+  const location = document.createElement('p');
+  location.textContent = object.location;
+  const profile = document.createElement('p');
+  profile.textContent = 'Profile:';
+  const gitLink = document.createElement('a');
+  gitLink.textContent = object.html_url;
+  const followers = document.createElement('p');
+  followers.textContent = `Followers: ${object.followers}`;
+  const following = document.createElement('p');
+  following.textContent = `Following: ${object.following}`;
+  const bio = document.createElement('p');
+  bio.textContent = `Bio: ${object.bio}`;
 
-  card.append(image)
-  card.append(cardInfo)
-  cardInfo.append(name)
-  cardInfo.append(userName)
-  cardInfo.append(location)
-  cardInfo.append(profile)
-  profile.append(gitLink)
-  cardInfo.append(followers)
-  cardInfo.append(following)
-  cardInfo.append(bio)
+  card.append(image);
+  card.append(cardInfo);
+  cardInfo.append(nameHeader);
+  cardInfo.append(userName);
+  cardInfo.append(location);
+  cardInfo.append(profile);
+  profile.append(gitLink);
+  cardInfo.append(followers);
+  cardInfo.append(following);
+  cardInfo.append(bio);
 
-}
+  return card;
+};
    /* <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
+   <img src={image url of user} />
+   <div class="card-info">
+     <h3 class="name">{users name}</h3>
+     <p class="username">{users user name}</p>
+     <p>Location: {users location}</p>
+     <p>Profile:
+       <a href={address to users github page}>{address to users github page}</a>
+     </p>
+     <p>Followers: {users followers count}</p>
+     <p>Following: {users following count}</p>
+     <p>Bio: {users bio}</p>
+   </div>
+ </div>
 */
 
 /*
